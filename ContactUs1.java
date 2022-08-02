db69d8e4801aba28915ae54931dafdeab3b842ec
7090620114 <110411076+7090620114@users.noreply.github.com>
2022-08-02 14:45:04 +0530

    Add files via upload

--- ContactUs1.java
+++ ContactUs1.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -24,6 +25,10 @@ public class ContactUs1 {
 	     String childId = it.next();
 	     driver.switchTo().window(childId);
 	     driver.manage().window().maximize();
+	     WebElement T = driver.findElement(By.xpath("//h3[contains(.,'Contact us')]"));
+	     Thread.sleep(2000);
+	     String text =T.getText();
+	     System.out.println(text+ " " +"Page is displayed");
 	    
 	}
 }

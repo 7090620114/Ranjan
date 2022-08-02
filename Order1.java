1ad48335e945de0cdddac60fe2ed2937330e1718
7090620114 <110411076+7090620114@users.noreply.github.com>
2022-08-02 10:30:03 +0530

    Add files via upload

--- Order1.java
+++ Order1.java
@@ -1,5 +1,9 @@
 package mobileworld1;
 
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -10,5 +14,13 @@ public class Order1 {
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 	   	 WebDriver driver = new ChromeDriver();
 	   	 driver.get("https://mobileworld.azurewebsites.net/");
+	   	 driver.manage().window().maximize();
+	     driver.findElement(By.xpath("//a[@href='mobile.html']")).click();
+	     driver.findElement(By.xpath("(//a[@href='order.html'])[2]")).click();
+	     Set<String> windows = driver.getWindowHandles();
+	     Iterator<String> it = windows.iterator();
+	     String parentId = it.next();
+	     String childId = it.next();
+	     driver.switchTo().window(childId);
 	}
 }
